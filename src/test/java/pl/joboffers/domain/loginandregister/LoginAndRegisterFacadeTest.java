a245f1457e6aaafa351a10b5b37c04bb33fcd753
krzysztofblaszczyk <Krzysi3kPGit>
2025-05-26 19:54:04 +0200

    Add packages and modules

---
+++ src/test/java/pl/joboffers/domain/loginandregister/LoginAndRegisterFacadeTest.java
@@ -0,0 +1,15 @@
+package pl.joboffers.domain.loginandregister;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LoginAndRegisterFacadeTest {
+    @Test
+    void test() {
+        //given
+        //when
+        //then
+
+    }
+}
---
+++ src/test/java/pl/joboffers/domain/offer/OfferFacadeTest.java
@@ -0,0 +1,21 @@
+package pl.joboffers.domain.offer;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OfferFacadeTest {
+
+    @Test
+    void shouldCreateOffer() {
+        // given
+
+
+        // when
+
+
+        // then
+
+
+    }
+}
