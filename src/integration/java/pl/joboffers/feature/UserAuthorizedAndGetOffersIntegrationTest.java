68aed5a52d88b9d1fc37ae2fb39e6e16a2c17182
krzysztofblaszczyk <Krzysi3kPGit>
2025-06-30 19:22:26 +0200

    adding one path to integration test

--- src/integration/java/pl/joboffers/feature/UserAuthorizedAndGetOffersIntegrationTest.java
+++ src/integration/java/pl/joboffers/feature/UserAuthorizedAndGetOffersIntegrationTest.java
@@ -6,7 +6,12 @@ import pl.joboffers.BaseIntegrationTest;
 
 public class UserAuthorizedAndGetOffersIntegrationTest extends BaseIntegrationTest {
     @Test
-    public void f(){
+    public void user_register_and_authorized_and_get_offers(){
+
+//        step1: User register to the system using POST /register with some username and some password
+//        step2: User get token from created account using GET /getToken/{userID}
+//        step3: User get offer by ID using GET /getOfferById/{offerID} with header "Authorization: {token}" and system returned OK(200) with offers
+//        step4: System returns requested offer from database
 
     }
 }
